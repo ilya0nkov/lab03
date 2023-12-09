7bfb1fadd30ff64fefb4becd2c4bb0ca1b8a450d
ilya <ilya.on2003@gmail.com>
2023-12-09 16:47:29 +0500

    add feature

--- main.py
+++ main.py
@@ -12,10 +12,18 @@ def bubble_sort(array):
 
 
 if __name__ == "__main__":
+    mode = int(input("Enter 1 if you want a direct order"
+                     "else if you want reverse order enter 2\n"))
+
     array = []
     n = int(input('Enter N\n'))
     for i in range(n):
         number = int(input('Enter number\n'))
         array.append(number)
     array = bubble_sort(array)
-    print("Sorted array:", array)
+
+    if mode == 1:
+        print("Sorted array:", array)
+    else:
+        print("Sorted array:", array[::-1])
+
